bc71a9815e1da8d1247133d07e35ff4d9d50d316
Tim Oxley <secoif@gmail.com>
2016-10-13 04:47:57 +0800

    [config deploy] Minor refactor.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,8 +2,13 @@
 
 set -e
 
-npm run build
-git diff-index --quiet HEAD -- || (echo '\n\n\x1B[37mPlease commit all changes before deploying!\x1B[0m e.g.\n\ngit add --force --all build && git commit -m "[build] Update build."\n\n'; git status; exit 1)
+needsCommit () {
+  echo '\n\n\x1B[37mPlease commit all changes before deploying!\x1B[0m e.g.\n\ngit add --force --all build && git commit -m "[build] Update build."\n\n'
+  git status
+  exit 1
+}
+
+git diff-index --quiet HEAD -- || needsCommit
 git branch -D master
 git subtree split --prefix build -b master
 git push -f origin master:master

397414f7344416bb94b0932f89514eb6cfff18a0
Tim Oxley <secoif@gmail.com>
2016-10-13 04:39:30 +0800

    [config deploy] Add more helpful message to deploy failure.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 set -e
 
 npm run build
-git diff-index --quiet HEAD -- || (echo 'Please commit all changes before deploying.'; exit 1)
+git diff-index --quiet HEAD -- || (echo '\n\n\x1B[37mPlease commit all changes before deploying!\x1B[0m e.g.\n\ngit add --force --all build && git commit -m "[build] Update build."\n\n'; git status; exit 1)
 git branch -D master
 git subtree split --prefix build -b master
 git push -f origin master:master
